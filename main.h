b5f32839083fd3b7357f1c1dac0a887d68a01f9e
root <jesus4388>
2022-03-12 19:22:46 -0800

    prueba

--- main.h
+++ main.h
@@ -3,7 +3,8 @@
 typedef struct op
 {
 	char c;
-		void (*f)(va_list p);
+	void (*f)(va_list p);
 }op_t;
 int _printf(const char *format, ...);
+int _putchar(char c);
 #endif
---
+++ prueba.c
@@ -0,0 +1,72 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ */
+void print_char(va_list f)
+{	
+	char a;
+
+	a = va_arg(f, int);
+	_putchar (a);
+}
+/**
+*/
+void print_string(va_list s)
+{
+	char *b = va_arg(s, char *);
+	int i = 0;
+
+	for (i = 0; b[i]; i++)
+		_putchar(b[i]);
+}
+/**
+ */
+void print_mod(va_list h)
+{	
+	_putchar('%');
+}
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	int j = 0;
+
+	va_list p;
+
+	op_t selector[] = {
+
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_mod},
+		{'\0', NULL}
+
+	};
+
+	va_start (p, format);
+
+	while (format[i] != '\0' && format[i] != '\0')
+	{
+		j = 0;
+		if (format[i] != '%')
+		{
+			_putchar (format[i]);
+			i++;
+		}
+		else
+		{
+			i++;
+			while (selector[j].c != '\0')
+			{
+				if (selector[j].c == format[i])
+				{
+					selector[j].f(p);
+					i++;
+				}
+			j++;
+			}
+		}
+	}
+	va_end(p);
+	return (i);
+}
