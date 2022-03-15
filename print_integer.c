d3d2bb326d0df5048d5b34433a60c1d3f608fed7
DaAsuncion <daniserafim_11@hotmail.com>
2022-03-15 12:20:49 -0700

    no modificar jamas

--- print_integer.c
+++ print_integer.c
@@ -11,49 +11,33 @@
  */
 int print_integer(va_list l)
 {
-	int aux;
-	int c = 1;
-	int i = 1;
+	int c = va_arg(l, int);
+	int j = 1;
+	int h = 0;
+	char *aux;
 
-	int n = va_arg(l, int);
+	h = c;
 
-	aux = n;
-	if (n < 0)
+	if (c < 0)
 	{
-		i = 2;
-		_putchar(45);
-		while (aux <= -10)
-		{
-			aux /= 10;
-			c *= 10;
-			i++;
-		}
-		for (; c >= 1; c /= 10)
-		{
-			_putchar((((n / c) % 10) * -1) + 48);
-		}
-	}
-	else
-	{
-<<<<<<< HEAD
 		_putchar('-');
 		c = (c * -1);
-<<<<<<< HEAD
-=======
-		i++;
->>>>>>> 483bbe0050103db2e3915e43a232b8c84a83bf93
-=======
-		while (aux >= 10)
-		{
-			aux /= 10;
-			c *= 10;
-			i++;
-		}
-		for (; c >= 1; c /= 10)
-		{
-			_putchar(((n / c) % 10) + 48);
-		}
->>>>>>> 3a8520ef5e80031816d840ecff97553ba4781f3f
 	}
-	return (i);
+	while (h / 10)
+	{
+		h = h / 10;
+		j++;
+	}
+	aux = malloc(j);
+	for (j = 0; c != 0; j++)
+	{
+		aux[j] = c % 10;
+		c = c / 10;
+	}
+	for (j = (strlen(aux) - 1); j >= 0; j--)
+	{
+		_putchar(aux[j] + '0');
+	}
+	free(aux);
+	return (strlen(aux) - 1);
 }
