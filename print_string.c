1c8091d1a92bbcabe31efb7d8d07463de0543125
root <jesus4388>
2022-03-15 07:08:02 -0700

    printf

--- print_string.c
+++ print_string.c
@@ -13,8 +13,18 @@ int print_string(va_list l)
 {
 	char *b = va_arg(l, char *);
 	int i = 0;
+	char c[] = {"(null)"};
 
-	for (i = 0; b[i]; i++)
-		_putchar(b[i]);
-	return (i);
+	if (b == NULL)
+	{
+		for (i = 0; c[i]; i++)
+			_putchar(c[i]);
+		return (i);
+	}
+	else
+	{
+		for (i = 0; b[i]; i++)
+			_putchar(b[i]);
+		return (i);
+	}
 }
