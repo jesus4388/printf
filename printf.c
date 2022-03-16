0b04836469ab5bab2effb94531b43c92ae1054d4
root <jesus4388>
2022-03-15 20:37:22 -0700

    update

--- printf.c
+++ printf.c
@@ -15,32 +15,34 @@ int _printf(const char *format, ...)
 		{'c', print_char}, {'s', print_string}, {'%', print_mod},
 		{'d', print_integer}, {'i', print_integer}, {'u', print_unsigned},
 		{'\0', NULL}};
+
 	va_start(p, format);
 	if (format == NULL)
 		return (-1);
 	for (i = 0; format != NULL && format[i] != '\0'; i++)
-	{	if (format[i] == '%')
-		{	flag = 1;
+	{
+		if (format[i] == '%')
+		{
+			flag = 1;
 			if (format[i + 1] == '\0')
 				return (-1);
-			return (-1);
 			i++;
 			for (j = 0; selector[j].c != '\0'; j++)
 				if (format[i] == selector[j].c)
-				{ 
 					s += selector[j].f(p);
-					flag = 0; 
-				}
+					flag = 0;
 		}
 		else
-		{	_putchar(format[i]);
+		{
+			_putchar(format[i]);
 			s++;
 			flag = 0;
-	       	}
+		}
 		if (flag == 1)
-		{	i--;
+		{
+			i--;
 			_putchar(37);
-			s++; 
+			s++;
 		}
 	}
 	return (s);
